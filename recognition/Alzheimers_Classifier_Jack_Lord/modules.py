92548ff980ab77701de68823205eb6b3f4fd88db
JRGL <jrglord201@gmail.com>
2025-10-04 17:56:00 +1000

    Updated structure and finished depthwise convolution part of __init__

--- recognition/Alzheimers_Classifier_Jack_Lord/modules.py
+++ recognition/Alzheimers_Classifier_Jack_Lord/modules.py
@@ -6,20 +6,30 @@ import torchvision.transforms as transforms
 import time
 
 class ConvnextBlock(nn.Module):
-    def __init__(self, in_dim, dim, kernel_size, stride=1, act_layer=nn.ReLU, norm_layer=nn.BatchNorm2d, dropout_p=0.3):
-        # Structure:
-        # Depthwise Conv2d
-        # Layer Norm
-        # Conv2d
-        # GELU
-        # Conv2d
-        # Layer Scale
-        # Drop Path
-
+    def __init__(self, ch_dw, kernel_size, stride=1, act_layer=nn.GELU, norm_layer=nn.LayerNorm, dropout_p=0.3):
         super().__init__()
-        self.features = nn.Conv2d(in_dim, dim, kernel_size=kernel_size, stride=stride, bias=True)
-        self.norm1 = norm_layer(dim)
+
+        # Structure:
+
+        # Depthwise Conv2d
+        self.depthwiseConv = nn.Conv2d(in_channels=ch_dw, out_channels=ch_dw, kernel_size=7, stride=1)
+
+        # Layer Norm
+        self.layer_norm = 
+
+        # Conv2d
+        self.conv_1 = 
+
+        # GELU
         self.act_layer = act_layer()
+
+        # Conv2d
+        self.conv_2 = 
+
+        # Layer Scale
+        self.layer_scale = 
+
+        # Drop Path
         self.dropout = nn.Dropout2d(p=dropout_p)
 
     def forward(self, x):
