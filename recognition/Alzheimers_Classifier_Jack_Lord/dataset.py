b3fa813078ef6a7b59cc8e0ec1c770f75e9cc884
JRGL <jrglord201@gmail.com>
2025-10-09 14:19:39 +1000

    Changed num_epochs to 3. and changed datasets to be subsets with 25% of the full folders' image contents.

--- recognition/Alzheimers_Classifier_Jack_Lord/dataset.py
+++ recognition/Alzheimers_Classifier_Jack_Lord/dataset.py
@@ -4,12 +4,24 @@ import torchvision
 import torchvision.transforms as transforms
 import time
 
-#Data
+torch.manual_seed(42)
 
+#Data
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
 
-trainset = torchvision.datasets.ImageFolder(root='recognition/Alzheimers_Classifier_Jack_Lord/ADNI/AD_NC/train', transform=transform)
-train_loader = torch.utils.data.DataLoader(trainset, batch_size=100, shuffle=True)
+full_trainset = torchvision.datasets.ImageFolder(root='recognition/Alzheimers_Classifier_Jack_Lord/ADNI/AD_NC/train', transform=transform)
 
-testset = torchvision.datasets.ImageFolder(root='recognition/Alzheimers_Classifier_Jack_Lord/ADNI/AD_NC/test', transform=transform)
-test_loader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False)
+train_subset_size = len(full_trainset)//4
+train_subset_indices = torch.randperm(len(full_trainset))[:train_subset_size]
+subset_trainset = torch.utils.data.Subset(full_trainset, train_subset_indices)
+
+train_loader = torch.utils.data.DataLoader(subset_trainset, batch_size=100, shuffle=True)
+
+
+full_testset = torchvision.datasets.ImageFolder(root='recognition/Alzheimers_Classifier_Jack_Lord/ADNI/AD_NC/test', transform=transform)
+
+test_subset_size = len(full_testset)//4
+test_subset_indices = torch.randperm(len(full_testset))[:test_subset_size]
+subset_testset = torch.utils.data.Subset(full_testset, test_subset_indices)
+
+test_loader = torch.utils.data.DataLoader(subset_testset, batch_size=100, shuffle=False)
--- recognition/Alzheimers_Classifier_Jack_Lord/train.py
+++ recognition/Alzheimers_Classifier_Jack_Lord/train.py
@@ -8,7 +8,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Using {device}")
 
 # Hyper-parameters
-num_epochs = 5
+num_epochs = 3
 learning_rate = 1e-3
 num_start_channels = 3
 num_classes = 2
