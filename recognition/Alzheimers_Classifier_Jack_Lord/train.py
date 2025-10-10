ab9cbf7bee98cab0ca800844b9f11472af8c25df
JRGL <jrglord201@gmail.com>
2025-10-10 14:49:36 +1000

    Changed lr to 1e-3 and changed num_epochs to 10

--- recognition/Alzheimers_Classifier_Jack_Lord/train.py
+++ recognition/Alzheimers_Classifier_Jack_Lord/train.py
@@ -8,8 +8,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Using {device}")
 
 # Hyper-parameters
-num_epochs = 5
-learning_rate = 1e-4
+num_epochs = 10
+learning_rate = 1e-3
 num_start_channels = 3
 num_classes = 2
 width = 256
