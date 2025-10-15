0062c5416b86ee49f7cd7a216818c17f65277ee8
JRGL <jrglord201@gmail.com>
2025-10-15 21:02:08 +1000

    Subsets = full sets size. epochs = 10

--- recognition/Alzheimers_Classifier_Jack_Lord/train.py
+++ recognition/Alzheimers_Classifier_Jack_Lord/train.py
@@ -9,7 +9,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Using {device}")
 
 # Hyper-parameters
-num_epochs = 50
+num_epochs = 10
 learning_rate = 4e-3
 num_start_channels = 3
 num_classes = 2
