5766fed47a5e93f80bf8650858deec1121885d53
JRGL <jrglord201@gmail.com>
2025-10-16 18:38:13 +1000

    changed drop prob to 0.3

--- recognition/Alzheimers_Classifier_Jack_Lord/modules.py
+++ recognition/Alzheimers_Classifier_Jack_Lord/modules.py
@@ -7,7 +7,7 @@ import time
 import random
 
 class ConvnextBlock(nn.Module):
-    def __init__(self, ch_dw, device, act_layer=nn.GELU, layer_scale_init_val=1e-6, drop_path_prob = 0.1):
+    def __init__(self, ch_dw, device, act_layer=nn.GELU, layer_scale_init_val=1e-6, drop_path_prob = 0.3):
         super().__init__()
 
         random.seed(42)
