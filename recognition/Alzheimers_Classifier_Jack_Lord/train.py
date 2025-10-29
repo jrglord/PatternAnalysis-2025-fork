47e735f4137687834300358bc46b50c78a297fbd
JRGL <jrglord201@gmail.com>
2025-10-29 14:16:17 +1000

    epochs = 6, gamma = 0.7

--- recognition/Alzheimers_Classifier_Jack_Lord/train.py
+++ recognition/Alzheimers_Classifier_Jack_Lord/train.py
@@ -19,7 +19,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Using {device}")
 
 # Hyper-parameters
-num_epochs = 12
+num_epochs = 6
 learning_rate = 1e-3
 num_start_channels = 3
 num_classes = 2
@@ -46,7 +46,7 @@ model = model.to(device)
 criterion = nn.CrossEntropyLoss()
 total_step = len(train_loader)
 optimiser = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=0.05)
-scheduler = StepLR(optimiser, step_size=1, gamma=0.15)
+scheduler = StepLR(optimiser, step_size=1, gamma=0.01)
 
 model.train()
 print("> Training")
