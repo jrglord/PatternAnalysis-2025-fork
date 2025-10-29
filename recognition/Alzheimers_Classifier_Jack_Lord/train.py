29d88e967352f97339ce11e95a091bf5101c1924
JRGL <jrglord201@gmail.com>
2025-10-29 15:08:18 +1000

    epochs now 15

--- recognition/Alzheimers_Classifier_Jack_Lord/train.py
+++ recognition/Alzheimers_Classifier_Jack_Lord/train.py
@@ -19,7 +19,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Using {device}")
 
 # Hyper-parameters
-num_epochs = 6
+num_epochs = 15
 learning_rate = 1e-3
 num_start_channels = 3
 num_classes = 2
