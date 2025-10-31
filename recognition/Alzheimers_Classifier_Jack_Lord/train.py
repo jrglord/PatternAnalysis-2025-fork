8042a0308442cc9f60784f92f3e92336877d159f
JRGL <jrglord201@gmail.com>
2025-10-31 23:22:49 +1000

    .

--- recognition/Alzheimers_Classifier_Jack_Lord/train.py
+++ recognition/Alzheimers_Classifier_Jack_Lord/train.py
@@ -21,7 +21,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Using {device}")
 
 # Hyper-parameters
-num_epochs = 10
+num_epochs = 7
 learning_rate = 5e-5
 num_start_channels = 3
 num_classes = 2
