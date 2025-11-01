b34ca33104f27011cb9dd840340079d53a0feb21
JRGL <jrglord201@gmail.com>
2025-11-01 21:15:48 +1000

    epochs = 35. datasets are 1/10th size

--- recognition/Alzheimers_Classifier_Jack_Lord/dataset.py
+++ recognition/Alzheimers_Classifier_Jack_Lord/dataset.py
@@ -27,7 +27,7 @@ full_trainset = torchvision.datasets.ImageFolder(root='recognition/Alzheimers_Cl
 # std /= len(train_loader_full)
 # print(f"mean: {mean}, std: {std}")
 
-train_subset_size = len(full_trainset)//1
+train_subset_size = len(full_trainset)//10
 train_subset_indices = torch.randperm(len(full_trainset))[:train_subset_size]
 subset_trainset = torch.utils.data.Subset(full_trainset, train_subset_indices)
 
@@ -36,7 +36,7 @@ train_loader = torch.utils.data.DataLoader(subset_trainset, batch_size=32, shuff
 
 full_testset = torchvision.datasets.ImageFolder(root='recognition/Alzheimers_Classifier_Jack_Lord/ADNI/AD_NC/test', transform=transform)
 
-test_subset_size = len(full_testset)//1
+test_subset_size = len(full_testset)//10
 test_subset_indices = torch.randperm(len(full_testset))[:test_subset_size]
 subset_testset = torch.utils.data.Subset(full_testset, test_subset_indices)
 
