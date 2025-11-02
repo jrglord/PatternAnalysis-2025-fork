66f05f2e46d356b67ae87ebaaf5724143cf79a49
JRGL <jrglord201@gmail.com>
2025-11-02 12:05:24 +1000

    Cleaned up dataset.py comments

--- recognition/Alzheimers_Classifier_Jack_Lord/dataset.py
+++ recognition/Alzheimers_Classifier_Jack_Lord/dataset.py
@@ -8,25 +8,11 @@ from torch.utils.data import ConcatDataset
 
 torch.manual_seed(42)
 
-#Data
+# Transforms for training dataset
 transform = transforms.Compose([transforms.ToTensor(),  transforms.RandomHorizontalFlip(p=0.3), transforms.RandomRotation(15)])
-# transforms.Resize(256), transforms.CenterCrop(224), transforms.Normalize(0.5, 0.5),
 
+# Training dataset and data loader
 full_trainset = torchvision.datasets.ImageFolder(root='recognition/Alzheimers_Classifier_Jack_Lord/ADNI/AD_NC/train', transform=transform)
-
-# full_trainset_B = torchvision.datasets.ImageFolder(root='recognition/Alzheimers_Classifier_Jack_Lord/ADNI/AD_NC/train', transform = transforms.ToTensor())
-# train_loader_full = torch.utils.data.DataLoader(full_trainset_B, batch_size=150, shuffle=False)
-# mean = 0
-# std = 0
-# i=1
-# for batch, _ in train_loader_full:
-#     print(i)
-#     i+=1
-#     mean += batch.mean()
-#     std += batch.std()
-# mean /= len(train_loader_full)
-# std /= len(train_loader_full)
-# print(f"mean: {mean}, std: {std}")
 
 train_subset_size = len(full_trainset)//1
 train_subset_indices = torch.randperm(len(full_trainset))[:train_subset_size]
@@ -34,7 +20,7 @@ subset_trainset = torch.utils.data.Subset(full_trainset, train_subset_indices)
 
 train_loader = torch.utils.data.DataLoader(subset_trainset, batch_size=32, shuffle=True)
 
-
+# Testing dataset and data loader
 full_testset = torchvision.datasets.ImageFolder(root='recognition/Alzheimers_Classifier_Jack_Lord/ADNI/AD_NC/test', transform=transforms.Compose([transforms.ToTensor()]))
 
 test_subset_size = len(full_testset)//1
